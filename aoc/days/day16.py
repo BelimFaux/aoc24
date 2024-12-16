3e7d4c0dd32eae7ff7baffcbbbda5e902c4bcec5
Felix Baum <baum-felix@gmx.de>
2024-12-16 21:07:02 +0100

    day 16 part 2

--- aoc/days/day16.py
+++ aoc/days/day16.py
@@ -1,5 +1,6 @@
+import heapq
 from pathlib import Path
-from collections import deque
+from collections import defaultdict
 from aoc.util import read, file, bench, env
 
 CURR_DAY: int = 16
@@ -26,48 +27,75 @@ class State:
 
     def actions(self, map: list[str]) -> list[tuple]:
         actions: list[tuple[State, int]] = []
+        if is_goal(map, self.pos):
+            return actions
 
         next_pos: tuple[int, int] = tuple_add(self.pos, DIR[self.dir])
         if map[next_pos[0]][next_pos[1]] != "#":
             actions.append((State(next_pos, self.dir), 1))
 
-        actions.append((State(self.pos, (self.dir + 1) % 4), 1000))
-        actions.append((State(self.pos, (self.dir - 1) % 4), 1000))
+        left: tuple[int, int] = tuple_add(self.pos, DIR[(self.dir + 1) % 4])
+        if map[left[0]][left[1]] != "#":
+            actions.append((State(self.pos, (self.dir + 1) % 4), 1000))
+        right: tuple[int, int] = tuple_add(self.pos, DIR[(self.dir - 1) % 4])
+        if map[right[0]][right[1]] != "#":
+            actions.append((State(self.pos, (self.dir - 1) % 4), 1000))
 
         return actions
 
     def __hash__(self) -> int:
         return hash(self.pos) + hash(self.dir)
 
-    def __eq__(self, value, /) -> bool:
-        return self.pos == value.pos and self.dir == value.dir
+    def __eq__(self, other, /) -> bool:
+        if isinstance(other, State):
+            return self.pos == other.pos and self.dir == other.dir
+        return False
+
+    # always false, so pq just uses the first tuple value
+    def __lt__(self, _) -> bool:
+        return False
+
+    def __str__(self) -> str:
+        return f"State {self.pos} / {self.dir}"
 
 
-def bfs(input: list[str]):
-    start = State((len(input) - 2, 1), 0)
-    parent = {start: None}
-    d = {start: 0}
+def all_dijkstra(input: list[str]):
+    start: State = State((len(input) - 2, 1), 0)
+    d: dict[State, int] = {start: 0}
+    paths: defaultdict[State, list[tuple[list[State], int]]] = defaultdict(
+        list, {start: [([start], 0)]}
+    )  # saves all paths to a node, as well as their length
 
-    q: deque = deque()
-    q.append(start)
-    in_queue = {start}
+    pq: list[tuple[int, State]] = [(0, start)]
+    visited: set[State] = set()
 
-    while q:
-        u = q.popleft()
-        in_queue.remove(u)
+    while pq:
+        du, u = heapq.heappop(pq)
+        if u in visited:
+            continue
+        visited.add(u)
 
         for n, cost in u.actions(input):
-            if n not in d or d[n] > d[u] + cost:
-                d[n] = d[u] + cost
-                parent[n] = u
+            dist: int = du + cost
+            if n not in d or dist < d[n]:
+                d[n] = dist
+                paths[n] = [(path + [n], dist) for path, _ in paths[u]]
+                heapq.heappush(pq, (d[n], n))
+            elif dist == d[n]:  # if cost is equal, create a new path
+                paths[n].extend([(p + [n], dist) for p, _ in paths[u]])
 
-                if n not in in_queue:
-                    q.append(n)
-                    in_queue.add(n)
+    m: int = min(c for n, c in d.items() if is_goal(input, n.pos))
 
-    cost = min(c for n, c in d.items() if is_goal(input, n.pos))
+    fields: set[tuple[int, int]] = set()
+    goals: list[State] = [State((1, len(input[0]) - 2), i) for i in range(4)]
+    for goal in goals:
+        for path, cost in paths[goal]:
+            if cost != m:
+                continue
+            for s in path:
+                fields.add(s.pos)
 
-    return cost, 0
+    return m, len(fields)
 
 
 def task2(input: tuple[int, int]) -> int:
@@ -83,12 +111,12 @@ def day16() -> None:
     test: list[str] = read.to_str_list(TEST_FILE_PATH)
     input: list[str] = read.to_str_list(INPUT_FILE_PATH)
 
-    res_test = bfs(test)
+    res_test: tuple[int, int] = all_dijkstra(test)
     print("test1:", task1(res_test))
     print("test2:", task2(res_test))
 
     if not ONLY_TESTS:
-        res_input = bfs(input)
+        res_input: tuple[int, int] = all_dijkstra(input)
         print("task1:", task1(res_input))
         print("task2:", task2(res_input))
 
